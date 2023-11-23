48c31183448664afff84065a6fcc63d6b8038339
Joaquin Rodriguez <JoaquindeveloperWeb@gmail.com>
2023-11-23 11:58:52 -0300

    se modifico un comentario

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -4,7 +4,7 @@ import './TaskList.css';
 
 const TaskList = ({currentTasks, deleteTask}) => {
   return (
-    // si el estado de las tareas no esta vacio se le agrega una clase al ul para que su display sea en grilla y el h2 que se renderiza si no hay tareas pueda abracar todo su ancho
+    // si el estado de las tareas no esta vacio se le agrega una clase al ul para que su display sea en grilla y el h2 que se renderiza si no hay tareas pueda abarcar todo su ancho
     <ul className={`taskListWrapper ${currentTasks!= 0 && 'gridTemplate'}`}>
       {
         currentTasks.length == 0
